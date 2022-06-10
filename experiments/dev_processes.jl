35ef0bd80ed7ba40460370a5106b65c67ba2d298
Gabrielle Queran <gabrielle.queran@gmail.com>
2022-06-10 15:49:23 +0200

    draft of new processes

---
+++ experiments/dev_processes.jl
@@ -0,0 +1,47 @@
+using Dolo
+# Bernouilli law
+
+struct Bernouilli <: Dolo.DiscreteProcess
+    Π::Float64
+end
+
+Bernouilli(;Π=0.5) = Bernouilli(Π)
+
+function discretize(bernouilli::Bernouilli)
+    x = reshape([0., 1.], 2, 1)
+    w = [1 - bernouilli.Π, bernouilli.Π]
+    return Dolo.DiscretizedIIDProcess(x, w)
+end
+
+discretize(Bernouilli(0.6))
+
+BernouilliSampler(bernouilli::Bernouilli, n::Int64) = Float64.(rand(n).> 1. - bernouilli.Π)
+BernouilliSampler(Bernouilli(),5)
+
+
+mutable struct Mixture <: Dolo.ContinuousProcess
+    index::Dolo.DiscreteProcess
+    distributions :: Tuple{<: Dolo.ContinuousProcess}
+end
+
+
+function discretize(self::Mixture)
+    inddist = discretize(self.index)
+    nodes = []
+    weights = []
+    for i in 1:(Dolo.n_inodes(inddist, 0)-1)
+        wind =  Dolo.iweight(inddist, 0, i)
+        # xind = inddist.inode(0, i)
+        dist = Dolo.discretize(self.distributions[i])
+        for j in 1:(Dolo.n_inodes(dist, 0))
+                w = Dolo.iweight(dist, 0, j)
+                x = Dolo.inode(dist, 0, j)
+                append!(nodes, x)
+                append!(weights, wind * w)
+        end
+    end
+    nodes = reshape(nodes, length(nodes), 1)
+    return Dolo.DiscretizedIIDProcess(nodes, weights)
+end
+
+discretize(Mixture(Bernouilli(0.1), tuple(Dolo.MvNormal(0.4))))
